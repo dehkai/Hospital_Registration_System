5713792d1691fb419a9c03fece507de7c3432d4b
dehkai <ydk1421@gmail.com>
2024-01-02 21:53:33 +0800

    Patient.java

---
+++ Hospital_Registration_System/src/Patient.java
@@ -0,0 +1,52 @@
+public class Patient extends User {
+    private String medicalRecordNumber, medicalRecord, InsuranceProvider;
+
+    public Patient(String medicalRecordNumber, String medicalRecord, String InsuranceProvider) {
+        this.medicalRecordNumber = medicalRecordNumber;
+        this.medicalRecord = medicalRecord;
+        this.InsuranceProvider = InsuranceProvider;
+    }
+
+    public String getMedicalRecordNumber() {
+        return medicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String medicalRecordNumber) {
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+
+    public String getMedicalRecord() {
+        return medicalRecord;
+    }
+
+    public void setMedicalRecord(String medicalRecord) {
+        this.medicalRecord = medicalRecord;
+    }
+
+    public String getInsuranceProvider() {
+        return InsuranceProvider;
+    }
+
+    public void setInsuranceProvider(String InsuranceProvider) {
+        this.InsuranceProvider = InsuranceProvider;
+    }
+
+    public void updatePatient() {
+
+    }
+
+    public void requestAppointment() {
+
+    }
+
+    public void viewAppointment() {
+
+    }
+
+    public void updateAppointment() {
+
+    }
+}
+    
+
+    
