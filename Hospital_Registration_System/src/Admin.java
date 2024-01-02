aee3c8bd209ea3147a13110d01c8d735a3a9c2d7
dehkai <ydk1421@gmail.com>
2024-01-02 22:21:38 +0800

    Admin.java

---
+++ Hospital_Registration_System/src/Admin.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Admin extends User{
+    
+    public void addDoctor(){
+        
+    }
+
+    public void removeDoctor(){
+        
+    }
+
+    public Doctor getDoctor(){
+        return null;
+    }
+
+    // public List<Appointment> getAppointment(){
+    //     return null;
+    // }
+
+    public void admitpatient(){
+        
+    }
+
+    public void dischargePatient(){
+        
+    }
+
+    public void addPatientRecord(){
+        
+    }
+}
